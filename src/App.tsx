624e4cbfb675677f0bb308c16177d3fb3bbd07e5
Farshid Ahmadi <ahmadi.fd@hotmail.com>
2023-12-19 18:25:27 +0330

    45

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-import { Route, Routes } from "react-router-dom";
-import Layout from "./components/Layout";
-import Public from "./components/Public";
-
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-      <Route index element={<Public />} />
-      </Route>
-    </Routes>
+    <>Hello World</>
+    // <Routes>
+    //   <Route path="/" element={<Layout />}>
+    //   <Route index element={<Public />} />
+    //   </Route>
+    // </Routes>
   );
 }
 
