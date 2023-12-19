a9ddc83b8b36d6189cc4aca53d98d8e0749268bf
Farshid Ahmadi <ahmadi.fd@hotmail.com>
2023-12-19 18:20:58 +0330

    1234

--- src/components/Public.tsx
+++ src/components/Public.tsx
@@ -6,17 +6,7 @@ const Public = () => {
   const content = (
     <>
       <Stack direction="column" mx={5}>
-        <Stack>
-          <Typography>Public-Header</Typography>
-        </Stack>
-        <Stack>
-          {" "}
-          <Typography>Public-Main</Typography>
-        </Stack>
-        <Stack>
-          <Typography>Public-Footer</Typography>
-         
-        </Stack>
+       Hello World
       </Stack>
     </>
   );
